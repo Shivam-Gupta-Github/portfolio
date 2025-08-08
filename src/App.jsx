ddbbdfff1b0e5192017da1bce026fadccae481e1
Shivam Gupta <shivamgupta35967@gmail.com>
2025-08-08 12:12:36 +0530

    added PageLoader

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import CTA from "./components/CTA";
 import Footer from "./components/Footer";
 import Skills from "./components/Skills";
 import Achievements from "./components/Achievements";
+import PageLoader from "./components/PageLoader";
 
 export default function App() {
   return (
     <div className="bg-white font-sans antialiased text-gray-900">
+      <PageLoader />
       <Header />
       <main>
         <Hero />
